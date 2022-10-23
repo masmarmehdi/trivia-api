5dac8482bc7e9aea14c25b918e87ad75145580b7
Mehdi Masmar <masmar507@gmail.com>
2022-10-23 23:19:27 +0100

    adding some additional tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,8 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format('@localhost:5432', self.database_name)
+        self.database_path = F"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -40,6 +40,27 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'], data['categories'])
 
+    def test_post_question(self):
+        question = {
+            'question': 'Who won the 2021/2022 english premier league?',
+            'answer': 'Mancity',
+            'difficulty': 3,
+            'category': 6
+        }
+        res = self.client().post('/questions', json=question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+    
+    def test_search(self):
+        search_term = {
+            'searchTerm': 'World'
+        }
+        res = self.client().post('/search', json=search_term)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'], len(data['questions']))
+    
     def test_questions_by_category(self):
         res = self.client().get('/categories/6/questions')
         data = json.loads(res.data)
@@ -48,6 +69,12 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['success'], data['questions'])
         self.assertTrue(data['total_questions'], data['current_category'])
 
+    def test_delete_question(self):
+        res = self.client().delete('/questions/3')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertEqual(data['deleted'], '3')
 
     def test_quiz(self):
         quiz_round = {

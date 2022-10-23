391406739776d69977eaf6c632a8852d3e8141ec
Mehdi Masmar <masmar507@gmail.com>
2022-10-23 20:29:04 +0100

    making some functions for testing

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres:Azertyuiop12//{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('postgres:Azertyuiop12@localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,7 +33,35 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'], data['categories'])
+
+    def test_questions_by_category(self):
+        res = self.client().get('/categories/6/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'], data['questions'])
+        self.assertTrue(data['total_questions'], data['current_category'])
+
+
+    def test_quiz(self):
+        quiz_round = {
+            'previous_questions': [],
+            'quiz_category': {
+                'type': 'Sports',
+                'id': 6
+                }
+        }
+        res = self.client().post('/quizzes', json=quiz_round)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'], data['question'])
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
